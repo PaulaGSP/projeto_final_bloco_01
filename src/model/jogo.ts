67aa8747f884fd295efefed20d46c116a237372a
Paula Policichio <113717928+PaulaGSP@users.noreply.github.com>
2025-07-25 19:58:29 -0300

    Update jogo.ts

--- src/model/jogo.ts
+++ src/model/jogo.ts
@@ -1,7 +1,7 @@
 import { Produto } from "./produto";
 
 export class Jogo extends Produto {
-  constructor(id: number, nome: string, preco: number, quantidade: number) {
+  constructor(id: number, nome: string, preco: number, genero: string, quantidade: number) {
     super(id, nome, preco, genero, quantidade);
   }
 }
